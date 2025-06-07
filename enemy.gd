2090f7e90fc13b9b5897647ff13ed2b25fd3d1d2
Huancke <38266019+Huancke@users.noreply.github.com>
2025-06-07 13:45:44 +0800

    进度更新

--- enemy.gd
+++ enemy.gd
@@ -1,10 +1,15 @@
 extends CharacterBody2D
 class_name enemy
-var speed=randf_range(5,20)
+var speed:float=randf_range(1,15)
 var player:Node2D=null
-var current_player=null
+var _player=null
 @onready var body=$body
-enum State{IDLE,MOVE,ATK,DEATH}
+enum state{
+	IDLE,
+	MOVE,
+	ATK,
+	DEATH,
+}
 #全局变量game.gd在查找player时报错，现在改为分组方法
 func _ready():
 	await get_tree().process_frame
@@ -12,37 +17,46 @@ func _ready():
 	if not found_player.is_empty():
 		player=found_player[0]
 		print("分组已找到：",player)
-var current_state=State.IDLE
+	print(body,"状态：",_state)
+var _state=state.IDLE
 func _physics_process(delta: float) -> void:
-	if current_state==State.DEATH||current_state==State.ATK:return
+	if _state==state.DEATH or _state==state.ATK:return
 	if player and is_instance_valid(player) and player is Node2D:
 		var direction = global_position.direction_to(player.global_position)
 		velocity = direction * speed
 		move_and_slide()
 		changeAnim()
 func changeAnim():
-	if velocity==Vector2.ZERO:
-		$body/AnimatedSprite2D.play(&"idle")
-		current_state=State.IDLE
+	if _state==state.ATK :return
+	elif velocity==Vector2.ZERO:
+		$body/AnimatedSprite2D.play("idle")
+		_state=state.IDLE
 	else:
-		$body/AnimatedSprite2D.play(&"move")
+		$body/AnimatedSprite2D.play("move")
+		_state=state.MOVE
 		await get_tree().create_timer(3.5).timeout
-		current_state=State.MOVE
 		body.scale.x=-1 if velocity.x<0 else 1
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("碰撞检测为：",body)
-	if body is Player:
-		current_player=body
-		current_state=State.ATK
-		$body/AnimatedSprite2D.play("atk")
-func _on_animated_sprite_2d_frame_changed() -> void:
-	if current_state==State.ATK && $body/AnimatedSprite2D.animation=='atk':
-		if current_player&&$body/AnimatedSprite2D.frame==2:pass
-func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body is Player && body==current_player:
-		current_player=null
-func _on_animated_sprite_2d_animation_finished() -> void:
-	if current_state==State.ATK && $body/AnimatedSprite2D.animation=='atk':
-		if current_player:$body/AnimatedSprite2D.play(&"atk")
-		else:current_state=State.MOVE
 #状态机未完成，需要重做
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	_state=state.ATK
+	body is enemy
+	_player=body
+	$body/AnimatedSprite2D.play("atk")
+	await $body/AnimatedSprite2D.animation_finished
+	_state=state.MOVE
+	$body/AnimatedSprite2D.play(&"move")
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if $body/AnimatedSprite2D.animation == "atk":
+		$body/AnimatedSprite2D.animation_finished
+		_state=state.MOVE
+		body is enemy
+		$body/AnimatedSprite2D.play(&"move")
+func _on_animated_sprite_2d_animation_changed() -> void:
+	if _state==state.ATK&&$body/AnimatedSprite2D.animation=='atk':
+		if _player&&$body/AnimatedSprite2D.frame==2:pass
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if _player:
+		$body/AnimatedSprite2D.play("atk")
+	if _state==state.ATK and $body/AnimatedSprite2D.animation=='atk':
+		_state=state.MOVE
+		$body/AnimatedSprite2D.play("move")
--- game.gd
+++ game.gd
@@ -1,6 +1,6 @@
 extends Node
 pass
-#课程中采用全局game脚本控制enemy的方法被弃用，改为分组方法
+#课程中采用全局game脚本控制enemy的方法因为难以查明的bug被弃用，改为分组方法
 #var player:Player=null
 #func _ready():
 #	await get_tree().process_frame
